1684473b824426b6516394eaae2fbab579d6709a
Jikku Jose <jikkujose@gmail.com>
2015-04-30 21:03:06 +0530

    Update content to test change

--- lib/simple_mvc.rb
+++ lib/simple_mvc.rb
@@ -3,7 +3,7 @@ require "simple_mvc/version"
 module SimpleMVC
   class Application
     def call(env)
-      [200, {"Content-Type" => "text/html"}, ['Hey, there! Its SimpleMVC']]
+      [200, {"Content-Type" => "text/html"}, ['Hello, there! Its SimpleMVC']]
     end
   end
 end
