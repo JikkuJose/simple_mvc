f9d0cc232c0f7c6f34c2aaaa1b292768d1a01daf
Jikku Jose <jikkujose@gmail.com>
2015-05-01 08:09:58 +0530

    Add contoller routing

--- lib/simple_mvc.rb
+++ lib/simple_mvc.rb
@@ -3,7 +3,26 @@ require "simple_mvc/version"
 module SimpleMVC
   class Application
     def call(env)
-      [200, {"Content-Type" => "text/html"}, ['Hello, there! Its SimpleMVC']]
+      @env = env
+      [200, {"Content-Type" => "text/html"}, response]
+    end
+
+    def response
+      controller.new.send action
+    end
+
+    def path_info
+      @env["PATH_INFO"].split("/").reject { |element| element.empty? }
+    end
+
+    def controller
+      controller_name = path_info.first
+      controller = (controller_name.capitalize + "Controller").to_sym
+      Object.const_get controller
+    end
+
+    def action
+      path_info.last.to_sym
     end
   end
 end
